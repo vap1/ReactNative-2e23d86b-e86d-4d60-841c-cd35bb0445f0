082c39e5c8e4622727f0e786bfc7ea7d5af5fe7c
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 09:34:50 +0530

    First Commit

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -1,25 +1,15 @@
 
 import axios from 'axios';
+import { UserRegistrationRequest, UserRegistrationResponse } from '../types/UserTypes';
 
-const BASE_URL = 'https://api.example.com'; // Replace with your actual API base URL
-
-export interface UserRegistrationRequest {
-  username: string;
-  password: string;
-  email: string;
-  role: string;
-}
-
-export interface UserRegistrationResponse {
-  success: boolean;
-  errorMessage?: string;
-}
+const BASE_URL = 'https://api.example.com'; // Replace with your API base URL
 
 export const registerUser = async (request: UserRegistrationRequest): Promise<UserRegistrationResponse> => {
   try {
-    const response = await axios.post<UserRegistrationResponse>(`${BASE_URL}/register`, request);
+    const response = await axios.post(`${BASE_URL}/register`, request);
     return response.data;
   } catch (error) {
-    throw new Error(error.response?.data?.errorMessage || 'An error occurred during user registration.');
+    console.error('Error registering user:', error);
+    throw error;
   }
 };
