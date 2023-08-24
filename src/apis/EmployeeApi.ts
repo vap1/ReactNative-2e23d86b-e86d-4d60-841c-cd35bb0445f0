e71f60bec7b248ea30d345c2cdc3e3b285c85324
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 09:34:57 +0530

    First Commit

--- src/apis/EmployeeApi.ts
+++ src/apis/EmployeeApi.ts
@@ -1,48 +1,35 @@
 
 import axios from 'axios';
+import { Employee, EmployeeResponse, EmployeeRequest } from '../types/EmployeeTypes';
 
-const BASE_URL = 'https://api.example.com'; // Replace with your actual API base URL
+const BASE_URL = 'https://api.example.com'; // Replace with your API base URL
 
-export interface Employee {
-  employeeId: string;
-  name: string;
-  contactInfo: string;
-  role: string;
-  permissions: string;
-}
-
-export interface EmployeeDetails {
-  // Define the fields for employee details here
-}
-
-export interface UserRegistrationResponse {
-  success: boolean;
-  errorMessage?: string;
-}
-
-export async function addEmployee(employeeDetails: EmployeeDetails): Promise<UserRegistrationResponse> {
+export const addEmployee = async (request: EmployeeRequest): Promise<EmployeeResponse> => {
   try {
-    const response = await axios.post<UserRegistrationResponse>(`${BASE_URL}/employees`, employeeDetails);
+    const response = await axios.post<EmployeeResponse>(`${BASE_URL}/employees`, request);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to add employee');
+    console.error('Error adding employee:', error);
+    throw error;
   }
-}
+};
 
-export async function editEmployee(employeeId: string, employeeDetails: EmployeeDetails): Promise<UserRegistrationResponse> {
+export const editEmployee = async (employeeId: string, request: EmployeeRequest): Promise<EmployeeResponse> => {
   try {
-    const response = await axios.put<UserRegistrationResponse>(`${BASE_URL}/employees/${employeeId}`, employeeDetails);
+    const response = await axios.put<EmployeeResponse>(`${BASE_URL}/employees/${employeeId}`, request);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to edit employee');
+    console.error('Error editing employee:', error);
+    throw error;
   }
-}
+};
 
-export async function removeEmployee(employeeId: string): Promise<UserRegistrationResponse> {
+export const removeEmployee = async (employeeId: string): Promise<EmployeeResponse> => {
   try {
-    const response = await axios.delete<UserRegistrationResponse>(`${BASE_URL}/employees/${employeeId}`);
+    const response = await axios.delete<EmployeeResponse>(`${BASE_URL}/employees/${employeeId}`);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to remove employee');
+    console.error('Error removing employee:', error);
+    throw error;
   }
-}
+};
