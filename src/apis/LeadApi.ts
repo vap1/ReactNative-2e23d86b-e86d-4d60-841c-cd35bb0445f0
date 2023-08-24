f3fcbb821e473cb8dbe3fd772c2a18b8a66f9efe
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 09:34:57 +0530

    First Commit

--- src/apis/LeadApi.ts
+++ src/apis/LeadApi.ts
@@ -1,45 +1,25 @@
 
 import axios from 'axios';
+import { Lead, LeadResponse, LeadRequest } from '../types/LeadTypes';
 
-const BASE_URL = 'https://api.example.com'; // Replace with your actual API base URL
+const BASE_URL = 'https://api.example.com'; // Replace with your API base URL
 
-export interface Lead {
-  leadId: string;
-  contactDetails: string;
-  relevantInfo: string;
-  assignedTo: string;
-  status: string;
-}
-
-export interface LeadDetails {
-  // Define the fields for lead details here
-}
-
-export interface UserRegistrationResponse {
-  success: boolean;
-  errorMessage?: string;
-}
-
-export class LeadApi {
-  static async addLead(leadDetails: LeadDetails): Promise<UserRegistrationResponse> {
-    try {
-      const response = await axios.post(`${BASE_URL}/leads`, leadDetails);
-      return response.data;
-    } catch (error) {
-      console.error('Error adding lead:', error);
-      throw error;
-    }
+export const addLead = async (request: LeadRequest): Promise<LeadResponse> => {
+  try {
+    const response = await axios.post<LeadResponse>(`${BASE_URL}/leads`, request);
+    return response.data;
+  } catch (error) {
+    console.error('Error adding lead:', error);
+    throw error;
   }
+};
 
-  static async updateLead(leadId: string, leadDetails: LeadDetails): Promise<UserRegistrationResponse> {
-    try {
-      const response = await axios.put(`${BASE_URL}/leads/${leadId}`, leadDetails);
-      return response.data;
-    } catch (error) {
-      console.error('Error updating lead:', error);
-      throw error;
-    }
+export const updateLead = async (leadId: string, request: LeadRequest): Promise<LeadResponse> => {
+  try {
+    const response = await axios.put<LeadResponse>(`${BASE_URL}/leads/${leadId}`, request);
+    return response.data;
+  } catch (error) {
+    console.error('Error updating lead:', error);
+    throw error;
   }
-
-  // Add more API methods for lead management as needed
-}
+};
