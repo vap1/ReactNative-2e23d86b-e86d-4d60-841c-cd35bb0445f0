21f38291ad1916206f70efdd04f06307546a5a17
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 10:52:15 +0530

    First Commit

--- src/screens/RegistrationScreen.tsx
+++ src/screens/RegistrationScreen.tsx
@@ -4,30 +4,42 @@ import { View, TextInput, Button, StyleSheet } from 'react-native';
 import { UserRegistrationRequest, UserRegistrationResponse } from '../types/UserTypes';
 import { registerUser } from '../apis/UserApi';
 
-interface RegistrationScreenProps {
-  // Add any necessary props here
-}
-
-const RegistrationScreen: React.FC<RegistrationScreenProps> = () => {
+const RegistrationScreen: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [role, setRole] = useState('');
 
-  const handleRegister = async () => {
-    try {
-      const request: UserRegistrationRequest = {
-        username,
-        password,
-        email,
-        role,
-      };
+  const handleUsernameChange = (text: string) => {
+    setUsername(text);
+  };
 
-      const response: UserRegistrationResponse = await registerUser(request);
+  const handlePasswordChange = (text: string) => {
+    setPassword(text);
+  };
+
+  const handleEmailChange = (text: string) => {
+    setEmail(text);
+  };
+
+  const handleRoleChange = (text: string) => {
+    setRole(text);
+  };
+
+  const handleSubmit = async () => {
+    const registrationRequest: UserRegistrationRequest = {
+      username,
+      password,
+      email,
+      role,
+    };
+
+    try {
+      const response: UserRegistrationResponse = await registerUser(registrationRequest);
       if (response.success) {
-        // Handle successful registration
+        // Registration successful, navigate to the next screen
       } else {
-        console.error('Error registering user:', response.errorMessage);
+        // Registration failed, display error message
       }
     } catch (error) {
       console.error('Error registering user:', error);
@@ -40,28 +52,28 @@ const RegistrationScreen: React.FC<RegistrationScreenProps> = () => {
         style={styles.input}
         placeholder="Username"
         value={username}
-        onChangeText={setUsername}
+        onChangeText={handleUsernameChange}
       />
       <TextInput
         style={styles.input}
         placeholder="Password"
         secureTextEntry
         value={password}
-        onChangeText={setPassword}
+        onChangeText={handlePasswordChange}
       />
       <TextInput
         style={styles.input}
         placeholder="Email"
         value={email}
-        onChangeText={setEmail}
+        onChangeText={handleEmailChange}
       />
       <TextInput
         style={styles.input}
         placeholder="Role"
         value={role}
-        onChangeText={setRole}
+        onChangeText={handleRoleChange}
       />
-      <Button title="Register" onPress={handleRegister} />
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
 };
