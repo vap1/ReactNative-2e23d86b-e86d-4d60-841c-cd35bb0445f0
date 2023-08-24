aad4a83b280bb4dd7abccb5e20f54ea85083aa76
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 10:36:49 +0530

    First Commit

--- src/components/OpportunityForm.tsx
+++ src/components/OpportunityForm.tsx
@@ -1,54 +1,44 @@
 
 import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
-import { OpportunityRequest } from '../types/OpportunityTypes';
-import { createOpportunity } from '../apis/OpportunityApi';
+import { Opportunity, OpportunityResponse } from '../types/OpportunityTypes';
+import { addOpportunity } from '../apis/OpportunityApi';
 
 interface OpportunityFormProps {
-  onSubmit: () => void;
+  onOpportunityAdded: (opportunity: Opportunity) => void;
 }
 
-const OpportunityForm: React.FC<OpportunityFormProps> = ({ onSubmit }) => {
+const OpportunityForm: React.FC<OpportunityFormProps> = ({ onOpportunityAdded }) => {
   const [leadId, setLeadId] = useState('');
   const [assignedTo, setAssignedTo] = useState('');
   const [status, setStatus] = useState('');
   const [notes, setNotes] = useState('');
   const [documents, setDocuments] = useState('');
 
-  const handleLeadIdChange = (text: string) => {
-    setLeadId(text);
-  };
-
-  const handleAssignedToChange = (text: string) => {
-    setAssignedTo(text);
-  };
-
-  const handleStatusChange = (text: string) => {
-    setStatus(text);
-  };
-
-  const handleNotesChange = (text: string) => {
-    setNotes(text);
-  };
-
-  const handleDocumentsChange = (text: string) => {
-    setDocuments(text);
-  };
-
-  const handleSubmit = async () => {
-    const opportunityRequest: OpportunityRequest = {
-      leadId,
-      assignedTo,
-      status,
-      notes,
-      documents,
-    };
-
+  const handleAddOpportunity = async () => {
     try {
-      await createOpportunity(opportunityRequest);
-      onSubmit();
+      const opportunity: Opportunity = {
+        opportunityId: '',
+        leadId,
+        assignedTo,
+        status,
+        notes,
+        documents,
+      };
+
+      const response: OpportunityResponse = await addOpportunity({ opportunity });
+      if (response.success) {
+        onOpportunityAdded(opportunity);
+        setLeadId('');
+        setAssignedTo('');
+        setStatus('');
+        setNotes('');
+        setDocuments('');
+      } else {
+        console.error('Error adding opportunity:', response.errorMessage);
+      }
     } catch (error) {
-      console.error('Error creating opportunity:', error);
+      console.error('Error adding opportunity:', error);
     }
   };
 
@@ -58,33 +48,33 @@ const OpportunityForm: React.FC<OpportunityFormProps> = ({ onSubmit }) => {
         style={styles.input}
         placeholder="Lead ID"
         value={leadId}
-        onChangeText={handleLeadIdChange}
+        onChangeText={setLeadId}
       />
       <TextInput
         style={styles.input}
         placeholder="Assigned To"
         value={assignedTo}
-        onChangeText={handleAssignedToChange}
+        onChangeText={setAssignedTo}
       />
       <TextInput
         style={styles.input}
         placeholder="Status"
         value={status}
-        onChangeText={handleStatusChange}
+        onChangeText={setStatus}
       />
       <TextInput
         style={styles.input}
         placeholder="Notes"
         value={notes}
-        onChangeText={handleNotesChange}
+        onChangeText={setNotes}
       />
       <TextInput
         style={styles.input}
         placeholder="Documents"
         value={documents}
-        onChangeText={handleDocumentsChange}
+        onChangeText={setDocuments}
       />
-      <Button title="Submit" onPress={handleSubmit} />
+      <Button title="Add Opportunity" onPress={handleAddOpportunity} />
     </View>
   );
 };
@@ -101,7 +91,7 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    marginBottom: 16,
+    marginBottom: 12,
     paddingHorizontal: 8,
   },
 });
