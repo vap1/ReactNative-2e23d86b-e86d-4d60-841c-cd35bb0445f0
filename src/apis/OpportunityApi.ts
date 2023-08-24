133cbfd3bd94c9e06c6e3d0704e475014aab536d
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 10:52:25 +0530

    First Commit

--- src/apis/OpportunityApi.ts
+++ src/apis/OpportunityApi.ts
@@ -1,35 +1,25 @@
 
 import axios from 'axios';
-import { Opportunity, OpportunityResponse, OpportunityRequest, OpportunityUpdateRequest } from '../types/OpportunityTypes';
+import { Opportunity, OpportunityResponse, OpportunityRequest } from '../types/OpportunityTypes';
 
 const BASE_URL = 'https://api.example.com'; // Replace with your API base URL
 
-export const addOpportunity = async (request: OpportunityRequest): Promise<OpportunityResponse> => {
+export const createOpportunity = async (request: OpportunityRequest): Promise<OpportunityResponse> => {
   try {
-    const response = await axios.post(`${BASE_URL}/opportunities`, request);
+    const response = await axios.post<OpportunityResponse>(`${BASE_URL}/opportunities`, request);
     return response.data;
   } catch (error) {
-    console.error('Error adding opportunity:', error);
+    console.error('Error creating opportunity:', error);
     throw error;
   }
 };
 
-export const updateOpportunity = async (opportunityId: string, updatedOpportunity: Opportunity): Promise<OpportunityResponse> => {
+export const updateOpportunity = async (opportunityId: string, request: OpportunityRequest): Promise<OpportunityResponse> => {
   try {
-    const response = await axios.put(`${BASE_URL}/opportunities/${opportunityId}`, { updatedOpportunity });
+    const response = await axios.put<OpportunityResponse>(`${BASE_URL}/opportunities/${opportunityId}`, request);
     return response.data;
   } catch (error) {
     console.error('Error updating opportunity:', error);
-    throw error;
-  }
-};
-
-export const deleteOpportunity = async (opportunityId: string): Promise<OpportunityResponse> => {
-  try {
-    const response = await axios.delete(`${BASE_URL}/opportunities/${opportunityId}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error deleting opportunity:', error);
     throw error;
   }
 };
