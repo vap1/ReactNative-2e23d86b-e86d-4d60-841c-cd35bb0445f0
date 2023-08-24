48961b46d8fcbbadf48ef5eee8431feb2d479aea
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 09:34:53 +0530

    First Commit

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,50 +1,68 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button, Alert } from 'react-native';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 interface RegistrationFormProps {
-  onSubmit: (username: string, password: string, email: string, role: string) => void;
+  onRegister: (username: string, password: string, email: string, role: string) => void;
 }
 
-const RegistrationForm: React.FC<RegistrationFormProps> = ({ onSubmit }) => {
+const RegistrationForm: React.FC<RegistrationFormProps> = ({ onRegister }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [role, setRole] = useState('');
 
-  const handleRegistration = () => {
-    // Perform validation here if needed
-
-    // Call the onSubmit function with the form data
-    onSubmit(username, password, email, role);
+  const handleRegister = () => {
+    onRegister(username, password, email, role);
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
+        style={styles.input}
         placeholder="Username"
         value={username}
         onChangeText={setUsername}
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
         secureTextEntry
         value={password}
         onChangeText={setPassword}
       />
       <TextInput
+        style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
+        style={styles.input}
         placeholder="Role"
         value={role}
         onChangeText={setRole}
       />
-      <Button title="Register" onPress={handleRegistration} />
+      <Button title="Register" onPress={handleRegister} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
 
 export default RegistrationForm;
