ee3711d9816a84c49e0488c6ef5bfb7e2b14bbb2
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 09:34:58 +0530

    First Commit

--- src/types/TaskTypes.ts
+++ src/types/TaskTypes.ts
@@ -6,11 +6,14 @@ export interface Task {
   comments: string;
 }
 
-export interface TaskDetails {
-  // Define the fields for task details here
-}
-
 export interface TaskResponse {
   success: boolean;
   errorMessage?: string;
+}
+
+export interface TaskRequest {
+  taskId: string;
+  assignedTo: string;
+  status: string;
+  comments: string;
 }
