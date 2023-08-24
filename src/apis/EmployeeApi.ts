4fd0eac8e5bfb8c0eb1dc0b9b59450fb9f57e4df
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 10:36:51 +0530

    First Commit

--- src/apis/EmployeeApi.ts
+++ src/apis/EmployeeApi.ts
@@ -1,12 +1,12 @@
 
 import axios from 'axios';
-import { Employee, EmployeeResponse, EmployeeRequest } from '../types/EmployeeTypes';
+import { Employee, EmployeeResponse, EmployeeRequest, EmployeeUpdateRequest } from '../types/EmployeeTypes';
 
 const BASE_URL = 'https://api.example.com'; // Replace with your API base URL
 
 export const addEmployee = async (request: EmployeeRequest): Promise<EmployeeResponse> => {
   try {
-    const response = await axios.post<EmployeeResponse>(`${BASE_URL}/employees`, request);
+    const response = await axios.post(`${BASE_URL}/employees`, request);
     return response.data;
   } catch (error) {
     console.error('Error adding employee:', error);
@@ -14,22 +14,22 @@ export const addEmployee = async (request: EmployeeRequest): Promise<EmployeeRes
   }
 };
 
-export const editEmployee = async (employeeId: string, request: EmployeeRequest): Promise<EmployeeResponse> => {
+export const updateEmployee = async (employeeId: string, updatedEmployee: Employee): Promise<EmployeeResponse> => {
   try {
-    const response = await axios.put<EmployeeResponse>(`${BASE_URL}/employees/${employeeId}`, request);
+    const response = await axios.put(`${BASE_URL}/employees/${employeeId}`, { updatedEmployee });
     return response.data;
   } catch (error) {
-    console.error('Error editing employee:', error);
+    console.error('Error updating employee:', error);
     throw error;
   }
 };
 
-export const removeEmployee = async (employeeId: string): Promise<EmployeeResponse> => {
+export const deleteEmployee = async (employeeId: string): Promise<EmployeeResponse> => {
   try {
-    const response = await axios.delete<EmployeeResponse>(`${BASE_URL}/employees/${employeeId}`);
+    const response = await axios.delete(`${BASE_URL}/employees/${employeeId}`);
     return response.data;
   } catch (error) {
-    console.error('Error removing employee:', error);
+    console.error('Error deleting employee:', error);
     throw error;
   }
 };
