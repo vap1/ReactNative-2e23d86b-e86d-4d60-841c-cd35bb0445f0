a1152cbf81a535783dedc0f7dc6e13385d43430a
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 09:34:49 +0530

    First Commit

--- src/types/OpportunityTypes.ts
+++ src/types/OpportunityTypes.ts
@@ -5,14 +5,19 @@ export interface Opportunity {
   assignedTo: string;
   status: string;
   notes: string;
-  documents: string[];
-}
-
-export interface OpportunityDetails {
-  // Define the fields for opportunity details here
+  documents: string;
 }
 
 export interface OpportunityResponse {
   success: boolean;
   errorMessage?: string;
+}
+
+export interface OpportunityRequest {
+  opportunityId: string;
+  leadId: string;
+  assignedTo: string;
+  status: string;
+  notes: string;
+  documents: string;
 }
