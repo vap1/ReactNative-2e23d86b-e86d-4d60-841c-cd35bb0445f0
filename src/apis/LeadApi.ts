aa1355bc46f75abfc5433acdce5c6262eb647c6b
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 10:36:52 +0530

    First Commit

--- src/apis/LeadApi.ts
+++ src/apis/LeadApi.ts
@@ -1,12 +1,12 @@
 
 import axios from 'axios';
-import { Lead, LeadResponse, LeadRequest } from '../types/LeadTypes';
+import { Lead, LeadResponse, LeadRequest, LeadUpdateRequest } from '../types/LeadTypes';
 
 const BASE_URL = 'https://api.example.com'; // Replace with your API base URL
 
 export const addLead = async (request: LeadRequest): Promise<LeadResponse> => {
   try {
-    const response = await axios.post<LeadResponse>(`${BASE_URL}/leads`, request);
+    const response = await axios.post(`${BASE_URL}/leads`, request);
     return response.data;
   } catch (error) {
     console.error('Error adding lead:', error);
@@ -14,12 +14,22 @@ export const addLead = async (request: LeadRequest): Promise<LeadResponse> => {
   }
 };
 
-export const updateLead = async (leadId: string, request: LeadRequest): Promise<LeadResponse> => {
+export const updateLead = async (leadId: string, updatedLead: Lead): Promise<LeadResponse> => {
   try {
-    const response = await axios.put<LeadResponse>(`${BASE_URL}/leads/${leadId}`, request);
+    const response = await axios.put(`${BASE_URL}/leads/${leadId}`, { updatedLead });
     return response.data;
   } catch (error) {
     console.error('Error updating lead:', error);
+    throw error;
+  }
+};
+
+export const deleteLead = async (leadId: string): Promise<LeadResponse> => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/leads/${leadId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error deleting lead:', error);
     throw error;
   }
 };
