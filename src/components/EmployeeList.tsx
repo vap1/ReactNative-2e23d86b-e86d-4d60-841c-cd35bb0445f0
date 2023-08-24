4da29d1a0b92ebd167c4d261c1c6b36a911a375c
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 10:52:26 +0530

    First Commit

--- src/components/EmployeeList.tsx
+++ src/components/EmployeeList.tsx
@@ -30,7 +30,6 @@ const EmployeeList: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Employee List</Text>
       <FlatList
         data={employees}
         renderItem={renderEmployee}
@@ -45,24 +44,19 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
   employeeContainer: {
     marginBottom: 12,
   },
   employeeName: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
   },
   employeeContact: {
-    fontSize: 16,
+    fontSize: 14,
+    color: 'gray',
   },
   employeeRole: {
-    fontSize: 16,
-    fontStyle: 'italic',
+    fontSize: 14,
   },
 });
 
